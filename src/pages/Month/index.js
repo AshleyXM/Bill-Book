eda18dee065d279f0900fbc666730a1d0e26fe05
AshleyXM <454198339@qq.com>
2024-06-23 23:38:02 -0700

    Add the pop-up window toggling effect

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,7 +1,17 @@
 import { NavBar, DatePicker } from "antd-mobile";
 import "./index.scss";
 
+import { useState } from "react";
+import classNames from "classnames";
+
 const Month = () => {
+  // 控制弹框的打开和关闭
+  const [dateVisible, setDateVisible] = useState(false);
+
+  const handleDataConfirm = () => {
+    setDateVisible(false);
+    // 其他处理逻辑
+  };
   return (
     <div className="monthlyBill">
       <NavBar className="nav" backArrow={false}>
@@ -10,9 +20,12 @@ const Month = () => {
       <div className="content">
         <div className="header">
           {/* 时间切换区域 */}
-          <div className="date">
+          <div className="date" onClick={() => setDateVisible(true)}>
             <span className="text">2023 | 3月账单</span>
-            <span className="arrow expand"></span>
+            {/* 根据当前弹框的打开状态控制expand类名的存在与否 */}
+            <span
+              className={classNames("arrow", dateVisible && "expand")}
+            ></span>
           </div>
           {/* 统计区域 */}
           <div className="twoLineOverview">
@@ -34,7 +47,10 @@ const Month = () => {
             className="kaDate"
             title="记账日期"
             precision="month"
-            visible={false}
+            visible={dateVisible}
+            onCancel={() => setDateVisible(false)}
+            onConfirm={handleDataConfirm}
+            onClose={() => setDateVisible(false)}
             max={new Date()}
           />
         </div>
